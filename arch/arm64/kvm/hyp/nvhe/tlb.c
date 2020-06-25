9cf976f641ad5e1cc06e1c98bd5fa1b001359d18
David Brazdil <dbrazdil@google.com>
2020-06-25 11:26:13 +0100

    arm64: kvm: Remove __hyp_text macro, use build rules  instead
    With nVHE code now fully separated from the rest of the kernel, the effects of
    the __hyp_text macro (which had to be applied on all nVHE code) can be
    achieved with build rules instead. The macro used to:
      (a) move code to .hyp.text ELF section, now done by renaming .text using
          `objcopy`, and
      (b) `notrace` and `__noscs` would negate effects of CC_FLAGS_FTRACE and
          CC_FLAGS_SCS, respectivelly, now those flags are  erased from
          KBUILD_CFLAGS (same way as in EFI stub).
    
    Note that by removing __hyp_text from code shared with VHE, all VHE code is now
    compiled into .text and without `notrace` and `__noscs`.
    
    Use of '.pushsection .hyp.text' removed from assembly files as this is now also
    covered by the build rules.
    
    For MAINTAINERS: if needed to re-run, uses of macro were removed with the
    following command. Formatting was fixed up manually.
    
      find arch/arm64/kvm/hyp -type f -name '*.c' -o -name '*.h' \
           -exec sed -i 's/ __hyp_text//g' {} +
    
    Signed-off-by: David Brazdil <dbrazdil@google.com>

--- arch/arm64/kvm/hyp/nvhe/tlb.c
+++ arch/arm64/kvm/hyp/nvhe/tlb.c
@@ -14,8 +14,7 @@ struct tlb_inv_context {
 	u64		sctlr;
 };
 
-static void __hyp_text __tlb_switch_to_guest(struct kvm *kvm,
-					     struct tlb_inv_context *cxt)
+static void __tlb_switch_to_guest(struct kvm *kvm, struct tlb_inv_context *cxt)
 {
 	if (cpus_have_final_cap(ARM64_WORKAROUND_SPECULATIVE_AT)) {
 		u64 val;
@@ -38,8 +37,7 @@ static void __hyp_text __tlb_switch_to_guest(struct kvm *kvm,
 	asm(ALTERNATIVE("isb", "nop", ARM64_WORKAROUND_SPECULATIVE_AT));
 }
 
-static void __hyp_text __tlb_switch_to_host(struct kvm *kvm,
-					    struct tlb_inv_context *cxt)
+static void __tlb_switch_to_host(struct kvm *kvm, struct tlb_inv_context *cxt)
 {
 	write_sysreg(0, vttbr_el2);
 
@@ -51,7 +49,7 @@ static void __hyp_text __tlb_switch_to_host(struct kvm *kvm,
 	}
 }
 
-void __hyp_text __kvm_tlb_flush_vmid_ipa(struct kvm *kvm, phys_addr_t ipa)
+void __kvm_tlb_flush_vmid_ipa(struct kvm *kvm, phys_addr_t ipa)
 {
 	struct tlb_inv_context cxt;
 
@@ -105,7 +103,7 @@ void __hyp_text __kvm_tlb_flush_vmid_ipa(struct kvm *kvm, phys_addr_t ipa)
 	__tlb_switch_to_host(kvm, &cxt);
 }
 
-void __hyp_text __kvm_tlb_flush_vmid(struct kvm *kvm)
+void __kvm_tlb_flush_vmid(struct kvm *kvm)
 {
 	struct tlb_inv_context cxt;
 
@@ -122,7 +120,7 @@ void __hyp_text __kvm_tlb_flush_vmid(struct kvm *kvm)
 	__tlb_switch_to_host(kvm, &cxt);
 }
 
-void __hyp_text __kvm_tlb_flush_local_vmid(struct kvm_vcpu *vcpu)
+void __kvm_tlb_flush_local_vmid(struct kvm_vcpu *vcpu)
 {
 	struct kvm *kvm = kern_hyp_va(kern_hyp_va(vcpu)->kvm);
 	struct tlb_inv_context cxt;
@@ -137,7 +135,7 @@ void __hyp_text __kvm_tlb_flush_local_vmid(struct kvm_vcpu *vcpu)
 	__tlb_switch_to_host(kvm, &cxt);
 }
 
-void __hyp_text __kvm_flush_vm_context(void)
+void __kvm_flush_vm_context(void)
 {
 	dsb(ishst);
 	__tlbi(alle1is);
