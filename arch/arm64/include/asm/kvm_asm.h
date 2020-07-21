464136dc7add4734dc11729a73e41639df40d208
Will Deacon <willdeacon@google.com>
2020-07-21 11:17:39 +0100

    Revert "arm64: kvm: Build hyp-entry.S separately for VHE/nVHE"
    This reverts commit 587b51f0689ffd5a95746114cf30c0b82315c683.
    
    Signed-off-by: Will Deacon <willdeacon@google.com>

--- arch/arm64/include/asm/kvm_asm.h
+++ arch/arm64/include/asm/kvm_asm.h
@@ -60,17 +60,12 @@
 	DECLARE_KVM_VHE_SYM(sym);		\
 	DECLARE_KVM_NVHE_SYM(sym)
 
-#define CHOOSE_VHE_SYM(sym)	sym
-#define CHOOSE_NVHE_SYM(sym)	kvm_nvhe_sym(sym)
-#define CHOOSE_HYP_SYM(sym)	(has_vhe() ? CHOOSE_VHE_SYM(sym) \
-					   : CHOOSE_NVHE_SYM(sym))
-
-/* Translate a kernel address @ptr into its equivalent linear mapping */
-#define kvm_ksym_ref(ptr)						\
+/* Translate a kernel address of @sym into its equivalent linear mapping */
+#define kvm_ksym_ref(sym)						\
 	({								\
-		void *val = (ptr);					\
+		void *val = &sym;					\
 		if (!is_kernel_in_hyp_mode())				\
-			val = lm_alias((ptr));				\
+			val = lm_alias(&sym);				\
 		val;							\
 	 })
 #define kvm_ksym_ref_nvhe(sym)	kvm_ksym_ref(kvm_nvhe_sym(sym))
@@ -81,14 +76,7 @@ struct kvm_vcpu;
 extern char __kvm_hyp_init[];
 extern char __kvm_hyp_init_end[];
 
-DECLARE_KVM_HYP_SYM(__kvm_hyp_vector);
-#define __kvm_hyp_vector	CHOOSE_HYP_SYM(__kvm_hyp_vector)
-
-#ifdef CONFIG_KVM_INDIRECT_VECTORS
-extern atomic_t arm64_el2_vector_last_slot;
-DECLARE_KVM_HYP_SYM(__bp_harden_hyp_vecs);
-#define __bp_harden_hyp_vecs	CHOOSE_HYP_SYM(__bp_harden_hyp_vecs)
-#endif
+extern char __kvm_hyp_vector[];
 
 extern void __kvm_flush_vm_context(void);
 extern void __kvm_tlb_flush_vmid_ipa(struct kvm *kvm, phys_addr_t ipa);
