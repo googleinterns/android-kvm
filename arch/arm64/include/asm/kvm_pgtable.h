ec17391c77237b9bedb8e89fc2dd2380829ee972
Quentin Perret <qperret@google.com>
2020-07-28 19:06:49 +0100

    KVM: arm64: Add support for stage-2 cache flushing in generic page-table
    Add support for cache flushing a range of the stage-2 address space to
    the generic page-table code.
    
    Cc: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/kvm_pgtable.h
+++ arch/arm64/include/asm/kvm_pgtable.h
@@ -227,6 +227,18 @@ kvm_pte_t kvm_pgtable_stage2_mkold(struct kvm_pgtable *pgt, u64 addr);
 bool kvm_pgtable_stage2_is_young(struct kvm_pgtable *pgt, u64 addr);
 
 /**
+ * kvm_pgtable_stage2_flush_range() - Clean and invalidate data cache to Point
+ * 				      of Coherency for guest stage-2 address
+ *				      range.
+ * @pgt:	Page-table structure initialised by kvm_pgtable_stage2_init().
+ * @addr:	Intermediate physical address from which to flush.
+ * @size:	Size of the range.
+ *
+ * Return: 0 on success, negative error code on failure.
+ */
+int kvm_pgtable_stage2_flush(struct kvm_pgtable *pgt, u64 addr, u64 size);
+
+/**
  * kvm_pgtable_walk() - Walk a page-table.
  * @pgt:	Page-table structure initialised by kvm_pgtable_*_init().
  * @addr:	Input address for the start of the walk.
