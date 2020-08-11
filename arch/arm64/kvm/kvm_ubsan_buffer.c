74c03e013575cbdc2afa38542e8ff9de50ec2bdb
George Popescu <georgepope@google.com>
2020-08-11 14:58:23 +0000

    KVM: arm64: Enable UBSAN_BOUNDS for the both the kernel and hyp/nVHE
    If an out of bounds happens inside the hyp/nVHE code, the ubsan_out_of_bounds
    handler stores the logging data inside the kvm_ubsan_buffer. The one responsible
    for printing is the kernel ubsan_out_of_bounds handler. The process of
    decapsulating the data from the buffer is straightforward.
    
    Signed-off-by: George Popescu <georgepope@google.com>

--- arch/arm64/kvm/kvm_ubsan_buffer.c
+++ arch/arm64/kvm/kvm_ubsan_buffer.c
@@ -18,6 +18,15 @@ DECLARE_KVM_DEBUG_BUFFER(struct kvm_ubsan_info, kvm_ubsan_buffer,
 		kvm_ubsan_buff_wr_ind, kvm_ubsan_buff_rd_ind,
 		KVM_UBSAN_BUFFER_SIZE);
 
+void __kvm_check_ubsan_data(struct kvm_ubsan_info *slot)
+{
+	switch (slot->type) {
+	case UBSAN_OUT_OF_BOUNDS:
+		__ubsan_handle_out_of_bounds(&slot->out_of_bounds_data,
+				slot->u_val.lval);
+		break;
+	}
+}
 
 void iterate_kvm_ubsan_buffer(unsigned long left, unsigned long right)
 {
@@ -31,7 +40,7 @@ void iterate_kvm_ubsan_buffer(unsigned long left, unsigned long right)
 		if (ind >=  KVM_UBSAN_BUFFER_SIZE) {
 			ind = 0;
 		}
-		/* check ubsan data */
+		__kvm_check_ubsan_data(slot + ind);
 		slot[i].type = 0;
 	}
 }
