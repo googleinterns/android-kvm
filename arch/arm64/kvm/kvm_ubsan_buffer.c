3db2677b5634a6bd1697f9ed98f3ba8be6e3fd0b
George Popescu <georgepope@google.com>
2020-08-11 08:46:14 +0000

    KVM: arm64: UBSAN check for unreachable code at EL2 enabled
    The data from __ubsan_handle_builtin_unreachable is passed to the buffer
    and printed at EL1 by its similar handler.
    
    Signed-off-by: George Popescu <georgepope@google.com>

--- arch/arm64/kvm/kvm_ubsan_buffer.c
+++ arch/arm64/kvm/kvm_ubsan_buffer.c
@@ -25,7 +25,10 @@ void __kvm_check_ubsan_data(struct kvm_ubsan_info *slot)
 		__ubsan_handle_out_of_bounds(&slot->out_of_bounds_data,
 				slot->u_val.lval);
 		break;
-	}
+	case UBSAN_UNREACHABLE_DATA:
+		__ubsan_handle_builtin_unreachable(&slot->unreachable_data);
+		break;
+    	}
 }
 
 void iterate_kvm_ubsan_buffer(int left, int right, unsigned long *nr_slots) {
