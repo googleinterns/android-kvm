16d2444fa31c40b344d2b1aaf7559acb226bcb8c
Quentin Perret <qperret@google.com>
2020-08-26 16:23:12 +0100

    KVM: arm64: Clean-up __kvm_bp_vect_base hyp usage
    __kvm_bp_vect_base is listed twice in image-vars.h, though only one
    alias is needed. Remove the redundant one, and while at it, change a
    comment to actually explain why the hyp needs a RW mapping on the host
    BSS.
    
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Will Deacon <willdeacon@google.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -1642,7 +1642,7 @@ static int init_hyp_mode(void)
 		goto out_err;
 	}
 
-	/* XXX - why turn RW again? */
+	/* XXX - use RW mappings to allow writing in __kvm_bp_vect_base */
 	err = create_hyp_mappings(kvm_ksym_ref(__hyp_bss_end),
 				  kvm_ksym_ref(__bss_stop), PAGE_HYP);
 	if (err) {
