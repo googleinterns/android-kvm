90c0b4179136643e9dad81df4c7dd4d8af0f98cd
George-Aurelian Popescu <georgepope@google.com>
2020-08-31 12:18:55 +0000

    KVM: arm64: Add a buffer that can pass UBSan data from hyp/nVHE to kernel
    Store data, which is collected from UBSan handlers that lives inside hyp/nVHE,
    into the kvm_ubsan_buffer.
    This buffer stores only UBSan data because it should not be preoccupied
    by other mechanisms data structures and functionalities.
    
    Also, for the moment the buffer is mapped inside .bss, where both the kernel
    and the hyp/nVHE code have Read/Write rights, but in the future this will change
    and the kernel will not be able to acess hyp/nVHE's .bss. At that point the buffer
    will only need to be mapped in order for this patch to work.

---
+++ arch/arm64/kvm/kvm_ubsan_buffer.c
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright 2020 Google LLC
+ * Author: George Popescu <georgepope@google.com>
+ */
+
+#include <linux/ctype.h>
+#include <linux/types.h>
+#include <asm/kvm_debug_buffer.h>
+#include <asm/kvm_arm.h>
+#include <asm/kvm_asm.h>
+#include <kvm/arm_pmu.h>
+
+#include <ubsan.h>
+#include <asm/kvm_ubsan.h>
+
+DECLARE_KVM_DEBUG_BUFFER(struct kvm_ubsan_info, kvm_ubsan_buffer,
+		kvm_ubsan_buff_wr_ind, kvm_ubsan_buff_rd_ind,
+		KVM_UBSAN_BUFFER_SIZE);
+
+
+void iterate_kvm_ubsan_buffer(unsigned long left, unsigned long right)
+{
+	unsigned long i;
+	unsigned long ind;
+	struct kvm_ubsan_info *slot;
+
+	slot = (struct kvm_ubsan_info *) this_cpu_ptr_nvhe(kvm_ubsan_buffer);
+	ind = left %  KVM_UBSAN_BUFFER_SIZE;
+	for (i = left; i < right; ++i, ++ind) {
+		if (ind >=  KVM_UBSAN_BUFFER_SIZE) {
+			ind = 0;
+		}
+		/* check ubsan data */
+		slot[i].type = 0;
+	}
+}
+
+void __kvm_check_ubsan_buffer(void)
+{
+	struct kvm_debug_buffer_ind crt;
+	unsigned long *write_ind, *read_ind;
+
+	write_ind = (unsigned long *) this_cpu_ptr_nvhe(kvm_ubsan_buff_wr_ind);
+	read_ind = this_cpu_ptr(&kvm_ubsan_buff_rd_ind);
+	crt = kvm_debug_buffer_get_ind(write_ind, read_ind, KVM_UBSAN_BUFFER_SIZE);
+	iterate_kvm_ubsan_buffer(crt.read, crt.write);
+}
+
